ae86f063e7752732bef9dad11f6cdb9e0dd4902d
avel moscare <avelqmoscare@gmail.com>
2018-07-26 16:12:06 +0800

    my first attemp

---
+++ machinehomeworklandscape/app/src/main/java/edu/sti/machinehomeworklandscape/landscape.java
@@ -0,0 +1,13 @@
+package edu.sti.machinehomeworklandscape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class landscape extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landscape);
+    }
+}
